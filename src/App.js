fb3643decdbb078ac5bbee70eee3fa58ae1facd5
vaibhav2699 <vaibhav.frontendarmy@gmail.com>
2021-06-21 16:58:47 +0530

    add home about and contactus pages and link them

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import {BrowserRouter as Router,Route,Switch,} from 'react-router-dom'; 
 import Header from "./component/Header";
 import Login from './component/Login';
+import About from './component/About';
+import Contact from './component/Contact';
+import Home from './component/Home';
 import Register from './component/Register';
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
       <Router>
         <switch>
           <Route exact path="/" component={Header}/>
+          <Route exact path="/Home" component={Home}/>
+          <Route exact path="Contact" component={Contact}/>
+          <Route exact path="About" component={About}/>
           <Route exact path="/Login" component={Login}/>
           <Route exact path="/Register" component={Register}/>
         </switch>
--- src/component/Header.js
+++ src/component/Header.js
@@ -5,16 +5,11 @@ class Header extends React.Component{
     render(){
         return(
             <div className="header1">
-                <button style={{margin:"21px"}}> Home </button>
-                <button style={{margin:"21px"}}> contact us </button>
-                <button style={{margin:"21px"}}> about us </button>
-                <Link to="Login">
-                    <button style={{margin:"21px"}}> Login </button>
-                </Link>
-                <Link to="Register">
-                    <button style={{margin:"21px"}}> Register </button>
-                </Link>
-                
+                <Link to="Home"><button style={{margin:"21px"}}> Home </button></Link>
+                <Link to="Contact"><button style={{margin:"21px"}}> contact us </button></Link>
+                <Link to="About"><button style={{margin:"21px"}}> about us </button></Link>
+                <Link to="Login"><button style={{margin:"21px"}}> Login </button></Link>
+                <Link to="Register"><button style={{margin:"21px"}}> Register </button></Link>
             </div>
         )
     }
